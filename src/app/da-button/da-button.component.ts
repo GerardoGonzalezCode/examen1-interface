b4af3106d06cda2ca9a732ca835ebcdd7d870c6b
Jerry <GerardoGonzalezCode@gmail.com>
2021-03-18 00:06:21 -0600

    examen1-interface v1.0.2

--- src/app/da-button/da-button.component.ts
+++ src/app/da-button/da-button.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Text } from '../text'
+import { Text } from '../text';
 import { DaButtonService } from './da-button.service';
 @Component({
   selector: 'app-da-button',
@@ -13,15 +13,12 @@ export class DaButtonComponent implements OnInit {
 
   text: Text = {
     text: "Hello little motherfucker",
-    sentiment: "joy"
   };
 
   constructor( private dService: DaButtonService) {}
 
   ngOnInit(): void {
-      this.dService.postText().subscribe( res => {
-        console.log('Res:', res);
-      })
+    this.dService.getPais();
   }
 
 }
